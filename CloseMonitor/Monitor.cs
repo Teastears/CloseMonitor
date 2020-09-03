8559f9d55adad76c6185465aae23f3b6b03b4eef
kang <86541947@qq.com>
2020-09-03 16:49:43 +0800

    添加到GitHub

---
+++ CloseMonitor/Monitor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CloseMonitor
+{
+    public static class Monitor
+    {
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+        public static void TurnOn()
+        {
+            SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, -1);
+        }
+        public static void TurnOff()
+        {
+            SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, 2);
+        }
+        static readonly IntPtr HWND_BROADCAST = new IntPtr(0xffff);
+        const uint WM_SYSCOMMAND = 0x0112;
+        const int SC_MONITORPOWER = 0xf170;
+    }
+}
---
+++ 关闭显示器/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace 关闭显示器
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Monitor is being shut off");
+            Thread.Sleep(1000);
+            CloseMonitor.Monitor.TurnOff();
+        }
+    }
+}
